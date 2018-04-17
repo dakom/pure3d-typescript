d47a36ef119ae75cb8d182e681da775093fa2303
dakom <david.komer@gmail.com>
2018-04-17 09:01:31 +0300

    submodule

--- example/build-utils/dev-file-server.js
+++ example/build-utils/dev-file-server.js
@@ -7,8 +7,8 @@ const cors = require('cors');
 
 let localPath = 
     os.platform() === "darwin"
-    ?   `/Users/davidk/Documents/github/KhronosGroup/glTF-Sample-Models/2.0`
-    :   `Y:\\KhronosGroup\\glTF-Sample-Models\\2.0`
+    ?   `/Users/davidk/Documents/github/dakom/pure3d/example/gltf-sample-models/2.0`
+    :   `Y:\\dakom\\pure3d\\example\\gltf-sample-models\\2.0`
 
 localPath = path.resolve(localPath);
 
