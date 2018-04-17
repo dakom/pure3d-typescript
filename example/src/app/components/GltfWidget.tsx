51b0aed420b4153b493cb945cc01a6624afb1d0f
dakom <david.komer@gmail.com>
2018-04-17 09:03:54 +0300

    production link

--- example/src/app/components/GltfWidget.tsx
+++ example/src/app/components/GltfWidget.tsx
@@ -9,7 +9,9 @@ import { MODEL, ModelContext, MODEL_CAMERA_POSITIONS, MODEL_ENVIRONMENT_EMPTY } 
 import {S, Maybe} from "../utils/Sanctuary";
 import { getDefaultPerspectiveProjection } from '../utils/Camera';
 
-const PRODUCTION_ASSET_PATH = "https://raw.githubusercontent.com/dakom/pure3d/example/gltf-sample-models/2.0/";
+const PRODUCTION_ASSET_PATH = "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/";
+//TODO: link to local assets on production build too
+//const PRODUCTION_ASSET_PATH = "https://raw.githubusercontent.com/dakom/pure3d/example/gltf-sample-models/2.0/";
 const DEV_ASSET_PATH = "http://localhost:4101/";
 
 class GltfDisplay extends React.Component<{path:string, model:MODEL}, {error?:any, isLoaded: boolean}> {
