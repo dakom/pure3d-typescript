274aa5838b1cf58404f705b141a62f0cfd772ec2
David Komer <david.komer@gmail.com>
2018-04-29 22:59:20 +0300

    raw link

--- examples/src/app/utils/Path.ts
+++ examples/src/app/utils/Path.ts
@@ -2,5 +2,5 @@
 export const GLTF_PRODUCTION_ASSET_PATH = "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/";
 export const GLTF_DEV_ASSET_PATH = "http://localhost:4101/glTF-Sample-Models/2.0/";
 
-export const WEBGL_PRODUCTION_ASSET_PATH = "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/";
+export const WEBGL_PRODUCTION_ASSET_PATH = "https://raw.githubusercontent.com/dakom/pure3d-example-assets/master/";
 export const WEBGL_DEV_ASSET_PATH = "http://localhost:4101/";
