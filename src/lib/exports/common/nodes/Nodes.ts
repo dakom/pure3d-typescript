278eda84347776c0c7618c1ce5cda41a0e883ade
David Komer <david.komer@gmail.com>
2018-05-28 07:31:47 +0300

    recursive map

--- src/lib/exports/common/nodes/Nodes.ts
+++ src/lib/exports/common/nodes/Nodes.ts
@@ -1,39 +1,32 @@
 import {updateTransform} from "../transform/Transform";
 import {NumberArray, TransformUpdateOptions, _Node} from "../../../Types";
 
+//Note - the immutability guarantee is the responsibility of the function - after it returns, the children are _replaced_
+export const mapNode = <T extends _Node> (fn: (node:T) => T) => (node:T):T => {
+    const n = fn (node)
+
+    return !n.children
+        ? n 
+        : Object.assign(n, {children: n.children.map(mapNode(fn))});
+
+}
 
 export const mapNodeWithParent = <T extends _Node> (fn:(parent:T) => (node:T) => T) => (parent:T) => (node:T):T => {
-   const _update = (_parent:T) => (_node:T):T => {
+    const n = fn (parent) (node)
 
-        const n = fn (_parent) (_node)
-
-        return !_node.children
-            ? n 
-            : Object.assign(n, {children: _node.children.map(_update (n))});
-    }
-
-    return _update(parent) (node);
+    return !n.children
+        ? n 
+        : Object.assign(n, {children: n.children.map(mapNodeWithParent (fn) (n))});
 }
 
 export const mapNodesWithParent = <T extends _Node> (fn:(parent:T) => (node:T) => T) => (parent:T) => (nodes:Array<T>):Array<T> => 
     nodes.map(mapNodeWithParent (fn) (parent));
 
-export const mapNode = <T extends _Node> (fn: (node:T) => T) => (node:T):T => {
-   const _update = (_node:T):T => {
-
-        const n = fn (_node)
-
-        return !_node.children
-            ? n 
-            : Object.assign(n, {children: _node.children.map(_update)});
-    }
-
-    return _update (node);
-}
 
 export const mapNodes = <T extends _Node> (fn: (node:T) => T) => (nodes:Array<T>):Array<T> => 
     nodes.map(mapNode(fn));
 
+//Specific use-cases
 export const updateNodeTransforms = <T extends _Node>(opts:TransformUpdateOptions) => (parent:T) => (node:T):T => 
     mapNodeWithParent((_parent:T) => (_node:T) => {
         const pModelMatrix = _parent ? _parent.transform.modelMatrix : undefined;
@@ -44,31 +37,5 @@ export const updateNodeTransforms = <T extends _Node>(opts:TransformUpdateOption
     (parent)
     (node);
 
-//export const updateNodeListTransforms = <T extends _Node>(opts:TransformUpdateOptions) => (parent:T) => (nodes:Array<T>):Array<T> => 
-    //nodes.map(updateNodeTransforms <T>(opts) (parent));
 export const updateNodeListTransforms = <T extends _Node>(opts:TransformUpdateOptions) => (parent:T) => (nodes:Array<T>):Array<T> => 
-    mapNodesWithParent((_parent:T) => (_node:T) => {
-        const pModelMatrix = _parent ? _parent.transform.modelMatrix : undefined;
-
-        const t = updateTransform(opts) (pModelMatrix) (_node.transform)
-        return Object.assign({}, _node, {transform: t});
-    })
-    (parent)
-    (nodes);
-/*
-export const updateNodeTransforms = <T extends _Node> (opts:TransformUpdateOptions) => (parentModelMatrix:NumberArray) => (node:T):T => {
-   const _update = <T extends _Node>(_parentModelMatrix:NumberArray) => (_node:T):T => {
-
-        const t = updateTransform(opts) (_parentModelMatrix) (_node.transform)
-
-        return !_node.children
-            ? Object.assign({}, _node, {transform: t}) 
-            : Object.assign({}, _node, {transform: t, children: _node.children.map(n => _update (t.modelMatrix) (n))});
-    }
-    return _update <T>(parentModelMatrix) (node)
-}
-
-
-export const updateNodeListTransforms = <T extends _Node>(opts:TransformUpdateOptions) => (parentModelMatrix:NumberArray) => (nodes:Array<T>):Array<T> =>
-    nodes.map(updateNodeTransforms <T>(opts) (parentModelMatrix));
-    */
+    mapNodesWithParent<T>(updateNodeTransforms (opts)) (parent) (nodes);
