fe9a98f8d9d774e651de59cca474ca758a4312b1
David Komer <david.komer@gmail.com>
2018-06-05 01:43:54 +0300

    replace shim

--- src/lib/internal/common/Json.ts
+++ src/lib/internal/common/Json.ts
@@ -1,8 +1,43 @@
-import {TextDecoder} from "text-encoding-shim";
+//https://stackoverflow.com/a/41798356/784519
+const utf8ArrayToStr = (function () {
+    var charCache = new Array(128);  // Preallocate the cache for the common single byte chars
+    var charFromCodePt = String.fromCodePoint || String.fromCharCode;
+    var result = [];
 
+    return function (array) {
+        var codePt, byte1;
+        var buffLen = array.length;
 
+        result.length = 0;
+
+        for (var i = 0; i < buffLen;) {
+            byte1 = array[i++];
+
+            if (byte1 <= 0x7F) {
+                codePt = byte1;
+            } else if (byte1 <= 0xDF) {
+                codePt = ((byte1 & 0x1F) << 6) | (array[i++] & 0x3F);
+            } else if (byte1 <= 0xEF) {
+                codePt = ((byte1 & 0x0F) << 12) | ((array[i++] & 0x3F) << 6) | (array[i++] & 0x3F);
+            } else if (String.fromCodePoint) {
+                codePt = ((byte1 & 0x07) << 18) | ((array[i++] & 0x3F) << 12) | ((array[i++] & 0x3F) << 6) | (array[i++] & 0x3F);
+            } else {
+                codePt = 63;    // Cannot convert four byte code points, so use "?" instead
+                i += 3;
+            }
+
+            result.push(charCache[codePt] || (charCache[codePt] = charFromCodePt(codePt)));
+        }
+
+        return result.join('');
+    };
+})();
 export const getJsonFromArrayBuffer = (aBuffer:ArrayBuffer) => {
-  const str = new TextDecoder('utf-8').decode(new Uint8Array(aBuffer));
+    const bytes = new Uint8Array(aBuffer);
+    
+    const str = TextDecoder
+        ?   new TextDecoder('utf-8').decode(bytes)
+        :   utf8ArrayToStr(bytes);
     //console.log(str);
 
   return JSON.parse(str);
